65bd398dde97784bd401aabcf5c47e024cdfcbad
Amajang <majangbahh@gmail.com>
2025-08-27 14:41:56 +0100

    Light and Dark theme Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div className="w-full min-h-screen rounded-md items-center bg-dark-100 ">
-      <div className="shadow-dark-100 radius-md flex items-center ">
-        <h1 className="text-4xl text-primary-100 font-bold">
-          Welcome to the world of Next js we are with Amajang
-        </h1>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
